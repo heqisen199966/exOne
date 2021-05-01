04c0735dd71fdf72cdd215002b0b527160231438
LemonJuice <1943285484@qq.com>
2021-05-01 20:51:06 +0800

    Initial commit

---
+++ src/hqs/Hello.java
@@ -0,0 +1,31 @@
+package hqs;
+
+import java.security.spec.RSAMultiPrimePrivateCrtKeySpec;
+
+public class Hello {
+
+    public String name;
+    public String aa;
+
+
+    public String getName() {for (int i = 0; i <; i++) {
+
+        }
+        System.out.println("aa" return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAa() {
+        return aa;
+    }
+
+    public void setAa(String aa) {
+        if () {
+            this.aa = aa;
+        }
+    }
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,31 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Core
+  Date: 2021/4/28/0028
+  Time: 12:15
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+<%--</head>--%>
+DDDD
+<body>
+
+
+
+
+ <div>
+
+</div><div>
+</div><div>
+</div><div>
+
+</div><div>
+
+</div><div>
+
+</div>
+  d
+  </body>
+</html>
